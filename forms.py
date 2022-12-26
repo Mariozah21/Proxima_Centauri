0a28e7132ae2ca7a857cfd9b74ea046d3611dc2d
xbusek1 <xbusek1@mendelu.cz>
2022-12-26 15:57:19 +0100

    login a register - vzhled

---
+++ forms.py
@@ -0,0 +1,6 @@
+from wtforms import Form, StringField, PasswordField, validators
+
+class SignInForm(Form):
+    login = StringField(name='login', label='Login', validators=[validators.Length(min=2, max=30), validators.InputRequired()])
+    password = PasswordField(name='password', label='Password', validators=[validators.Length(min=3), validators.InputRequired()])
+    
