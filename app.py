75af8818835524b9711587c3f239423cfdde8dee
xbusek1 <xbusek1@mendelu.cz>
2023-01-08 01:43:04 +0100

    logout

--- app.py
+++ app.py
@@ -74,5 +74,12 @@ def view_mybase_page():
 def view_about_us():
     return render_template("aboutus.jinja")
 
+@app.route('/logout')
+@auth.login_required
+def logout():
+    session.pop("authenticated")
+    session.pop("email")
+    return redirect(url_for('index'))
+
 
 app.run('0.0.0.0', port=5000)
