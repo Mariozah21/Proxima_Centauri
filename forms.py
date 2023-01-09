d6689fd2666f28326a35a7d70b334fe06b55b153
xbusek1 <xbusek1@mendelu.cz>
2023-01-09 23:19:27 +0100

    Uprava profilu lepsi

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
-from wtforms import Form, PasswordField, StringField, EmailField, IntegerField, DecimalField, RadioField, validators
+from wtforms import Form, PasswordField, StringField, IntegerField, DecimalField, RadioField, validators
 class RegisterUserForm(Form):
     meno = StringField(name='meno',label='Meno',validators=[validators.length(min=2,max=50), validators.InputRequired()])
     priezvisko = StringField(name='priezvisko',label='Priezvisko',validators=[validators.length(min=2,max=100), validators.InputRequired()])
     pohlavie = StringField(name='pohlavie',label='Pohlavie',validators=[validators.length(min=1,max=5), validators.InputRequired()])
-    email = EmailField(name='email',label='Email',validators=[validators.length(min=2,max=100), validators.InputRequired(), validators.Email()])
+    email = StringField(name='email',label='Email',validators=[validators.length(min=2,max=100), validators.InputRequired()])
     heslo = PasswordField(name='heslo',label='Heslo',validators=[validators.length(min=5,max=100), validators.InputRequired()])
     zakladne_id_zakladne = RadioField(name='zakladne_id_zakladne', label='Zakladna', choices = [('1','Merkur01'),('2','Venusa02'), ('3','Zem03'),('4','Mars04'), ('5','Jupiter05'),('6','Saturn06'), ('7','Uran07'),('8','Neptun08')], default=1)
 
@@ -19,12 +19,12 @@ class NewBaseForm(Form):
     medzi_galakticke_kredity = IntegerField(name='medzi_galakticke_kredity',label='Medzi_galakticke_kredity',validators=[validators.InputRequired()])
 
 class UserMoveForm(Form):
-    email = EmailField(name='email',label='Email uzivatela',validators=[validators.length(min=2,max=100), validators.InputRequired(), validators.Email()])
+    email = StringField(name='email',label='Email uzivatela',validators=[validators.length(min=2,max=100), validators.InputRequired()])
     heslo = PasswordField(name='heslo', label='Heslo uzivatela', validators=[validators.Length(min=3), validators.InputRequired()])
     zakladne_id_zakladne = IntegerField(name='zakladne_id_zakladne',label='Nova zakladna uzivatela',validators=[validators.InputRequired()])
  
 class NameUserForm(Form):
-    email = EmailField(name='email',label='Email uzivatela',validators=[validators.length(min=2,max=100), validators.InputRequired(), validators.Email()])
+    email = StringField(name='email',label='Email uzivatela',validators=[validators.length(min=2,max=100), validators.InputRequired()])
     heslo = PasswordField(name='heslo', label='Heslo uzivatela', validators=[validators.Length(min=3), validators.InputRequired()])
 
     
@@ -42,5 +42,5 @@ class ChangeHesloForm(Form):
     noveheslo = PasswordField(name='noveheslo',label='Nove Heslo',validators=[validators.length(min=5,max=100), validators.InputRequired()])
 
 class ChangeEmailForm(Form):
-    novyemail = EmailField(name='novyemail',label='Novy Email',validators=[validators.length(min=2,max=100), validators.InputRequired(), validators.Email()])
+    novyemail = StringField(name='novyemail',label='Novy Email',validators=[validators.length(min=2,max=100), validators.InputRequired()])
     heslo = PasswordField(name='heslo',label='Heslo',validators=[validators.length(min=5,max=100), validators.InputRequired()])
